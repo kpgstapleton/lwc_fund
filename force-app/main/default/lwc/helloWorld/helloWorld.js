c387b4993edb22de95d345ce172a51f73c3687c2
Kevin Stapleton <kpgstapleton@gmail.com>
2021-03-08 09:43:27 +0000

    2 way data binding

--- force-app/main/default/lwc/helloWorld/helloWorld.js
+++ force-app/main/default/lwc/helloWorld/helloWorld.js
@@ -1,3 +1,10 @@
 import { LightningElement } from 'lwc';
 
-export default class HelloWorld extends LightningElement {}
+export default class HelloWorld extends LightningElement {
+    fullName = "John Smith";
+    title = "aura";
+    //method
+    changeHandler(event){
+        this.title = event.target.value;
+    }
+}
