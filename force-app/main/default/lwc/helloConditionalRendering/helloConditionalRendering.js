90a08c6b8aebcc93af9d777c549c8b2e4f5777d1
Kevin Stapleton <kpgstapleton@gmail.com>
2021-03-11 09:46:52 +0000

    looping example

--- force-app/main/default/lwc/helloConditionalRendering/helloConditionalRendering.js
+++ force-app/main/default/lwc/helloConditionalRendering/helloConditionalRendering.js
@@ -2,8 +2,17 @@ import { LightningElement } from 'lwc';
 
 export default class HelloConditionalRendering extends LightningElement {
   isVisible = false;
+  name = "";
 
   handleClick(){
     this.isVisible = true;
+  }
+
+  changeHandler(event){
+    this.name = event.target.value;
+  }
+
+  get helloMethod(){
+    return this.name === 'hello';
   }
 }
