7978254fdb45099f621e1c2eba5f78278c050ac7
Kevin Stapleton <kpgstapleton@gmail.com>
2021-03-12 17:30:19 +0000

    iterator loop example

--- force-app/main/default/lwc/looping/looping.js
+++ force-app/main/default/lwc/looping/looping.js
@@ -3,5 +3,28 @@ import { LightningElement } from 'lwc';
 export default class Looping extends LightningElement {
 
   carList = ["Ford", "Audi", "Seat", "Mercedes", "Kia"];
+
+  ceoList = [
+    {
+      id:1,
+      company:"Google",
+      name:"Sundar Pichai"
+    },
+    {
+      id:2,
+      company:"Apple Inc",
+      name:"Tim Cook"
+    },
+    {
+      id:3,
+      company:"Facebook",
+      name:"Mark Zuckerberg"
+    },
+    {
+      id:4,
+      company:"Amazon.com",
+      name:"Jeff Bezos"
+    }
+  ]
   
 }
