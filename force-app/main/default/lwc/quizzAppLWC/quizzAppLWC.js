b05fb7628534971a126b16420417dd11660386a9
Kevin Stapleton <kpgstapleton@gmail.com>
2021-03-15 09:09:36 +0000

    output questions in html

--- force-app/main/default/lwc/quizzAppLWC/quizzAppLWC.js
+++ force-app/main/default/lwc/quizzAppLWC/quizzAppLWC.js
@@ -1,3 +1,36 @@
 import { LightningElement } from 'lwc';
 
-export default class QuizzAppLWC extends LightningElement {}
+export default class QuizzAppLWC extends LightningElement {
+  myQuestions=[
+    {
+      id:"Question 1",
+      question: "Which of the following is not a template loop?",
+      answers:{
+        a:"for:each",
+        b:"iterator",
+        c:"map loop"
+      },
+      correctAnswer:"c"
+    },
+    {
+      id:"Question 2",
+      question: "Which file is invalid in the LWC Component folder?",
+      answers:{
+        a:".svg",
+        b:".apex",
+        c:".js"
+      },
+      correctAnswer:"b"
+    },
+    {
+      id:"Question 3",
+      question: "Which of the following is not a directive?",
+      answers:{
+        a:"for:each",
+        b:"if:true",
+        c:"@track"
+      },
+      correctAnswer:"c"
+    }
+  ]
+}
