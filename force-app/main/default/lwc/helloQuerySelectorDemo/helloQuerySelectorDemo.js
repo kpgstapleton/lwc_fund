f1b4bb5739b3b1d1d41644e3c429a435aa3686d4
Kevin Stapleton <kpgstapleton@gmail.com>
2021-03-17 20:52:03 +0000

    query selector all demo

--- force-app/main/default/lwc/helloQuerySelectorDemo/helloQuerySelectorDemo.js
+++ force-app/main/default/lwc/helloQuerySelectorDemo/helloQuerySelectorDemo.js
@@ -1,10 +1,13 @@
 import { LightningElement } from 'lwc';
 
 export default class HelloQuerySelectorDemo extends LightningElement {
+  userNames = ['asd', '2sds', 'cvxzfd','cdcd'];
 
   fetchDetailHandler(){
     const elem = this.template.querySelector('h1');
+    const userElements = this.template.querySelectorAll('.name'); //get the usernames listed in the class 'name'
     console.log(elem.innerText);
+    Array.from(userElements).forEach(item=>{console.log(item.innerText)}); //userElements is a node so convert that to a proper array and then console.log that
   }
 
 }
