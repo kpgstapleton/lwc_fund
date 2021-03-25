321be6c36b01475c8f7d4d88e8b17f3a3773170c
Kevin Stapleton <kpgstapleton@gmail.com>
2021-03-25 19:48:38 +0000

    lifecycle demo

---
+++ force-app/main/default/lwc/lifeCycleParent/lifeCycleParent.js
@@ -0,0 +1,21 @@
+import { LightningElement } from 'lwc';
+
+export default class LifeCycleParent extends LightningElement {
+  constructor(){
+    super()
+    console.log('parent constructor called')
+  }
+
+  connectedCallback(){
+
+    console.log('parent connected callback called')
+  }
+
+  renderedCallback(){
+    console.log('parent rendered callback called')
+  }
+  name
+  changeHandler(event){
+    this.name = event.target.value;
+  }
+}
