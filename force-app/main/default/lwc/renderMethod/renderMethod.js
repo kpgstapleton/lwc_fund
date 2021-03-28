10bf7856ffd429e624fc52c3c33c54e05b841a1f
Kevin Stapleton <kpgstapleton@gmail.com>
2021-03-28 10:22:13 +0100

    render method example

---
+++ force-app/main/default/lwc/renderMethod/renderMethod.js
@@ -0,0 +1,16 @@
+import { LightningElement } from 'lwc';
+import signinTemplate from './signinTemplate.html';
+import signupTemplate from './signupTemplate.html';
+import renderTemplate from './renderMethod.html';
+
+export default class RenderMethod extends LightningElement {
+  selectedButton='';
+
+  render(){
+    return this.selectedButton === 'Signup' ? signupTemplate : this.selectedButton === 'Signin' ? signinTemplate : renderTemplate;
+  }
+
+  handleClick(event){
+    this.selectedButton = event.target.label;
+  }
+}
