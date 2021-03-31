c7f96f91f580cb876c7ea375033a4ff0dfe8cac0
Kevin Stapleton <kpgstapleton@gmail.com>
2021-03-31 10:05:39 +0100

    p2c parent to child component pass data on action

--- force-app/main/default/lwc/p2cParentComponent/p2cParentComponent.js
+++ force-app/main/default/lwc/p2cParentComponent/p2cParentComponent.js
@@ -3,6 +3,11 @@ import { LightningElement, api } from 'lwc';
 export default class P2cParentComponent extends LightningElement {
   // @api message
   // @api cardHeading
+  percentage = 10;
+  
+  changeHandler(event){
+    this.percentage = event.target.value;
+  }
 
   carouselData=[
     {
