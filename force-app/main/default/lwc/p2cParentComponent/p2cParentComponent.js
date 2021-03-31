3ce026125a9e261fcfcc0891c527964c007dbdd1
Kevin Stapleton <kpgstapleton@gmail.com>
2021-03-31 09:48:48 +0100

    p2c non primitive data example

--- force-app/main/default/lwc/p2cParentComponent/p2cParentComponent.js
+++ force-app/main/default/lwc/p2cParentComponent/p2cParentComponent.js
@@ -3,4 +3,25 @@ import { LightningElement, api } from 'lwc';
 export default class P2cParentComponent extends LightningElement {
   // @api message
   // @api cardHeading
+
+  carouselData=[
+    {
+      src:"https://www.lightningdesignsystem.com/assets/images/carousel/carousel-01.jpg",
+      header:"First Card",
+      description:"First card description.",
+      "alternative-text": "text"
+    },
+    {
+      src:"https://www.lightningdesignsystem.com/assets/images/carousel/carousel-02.jpg",
+      header:"Second Card",
+      description:"Second card description.",
+      "alternative-text": "text"
+    },
+    {
+      src:"https://www.lightningdesignsystem.com/assets/images/carousel/carousel-03.jpg",
+      header:"Third Card",
+      description:"Third card description.",
+      "alternative-text": "text"
+    }
+  ]
 }
