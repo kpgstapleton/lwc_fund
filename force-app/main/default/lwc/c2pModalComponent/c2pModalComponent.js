cad538a4c73e8d87b45e781e8f4b277a8951a62b
Kevin Stapleton <kpgstapleton@gmail.com>
2021-04-07 10:27:24 +0100

    c2p event with data example

--- force-app/main/default/lwc/c2pModalComponent/c2pModalComponent.js
+++ force-app/main/default/lwc/c2pModalComponent/c2pModalComponent.js
@@ -2,7 +2,9 @@ import { LightningElement } from 'lwc';
 
 export default class C2pModalComponent extends LightningElement {
   closeHandler(){
-    const myEvent = new CustomEvent('close');
+    const myEvent = new CustomEvent('close', {
+      detail: "modal closed successfully"
+    });
     this.dispatchEvent(myEvent);
   }
 }
