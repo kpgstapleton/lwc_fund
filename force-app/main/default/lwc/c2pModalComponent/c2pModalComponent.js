72712bfe96103d32645f830405eabeb532c4407c
Kevin Stapleton <kpgstapleton@gmail.com>
2021-04-07 10:35:05 +0100

    c2p bubbling example

--- force-app/main/default/lwc/c2pModalComponent/c2pModalComponent.js
+++ force-app/main/default/lwc/c2pModalComponent/c2pModalComponent.js
@@ -3,8 +3,13 @@ import { LightningElement } from 'lwc';
 export default class C2pModalComponent extends LightningElement {
   closeHandler(){
     const myEvent = new CustomEvent('close', {
+      bubbles: true,
       detail: "modal closed successfully"
     });
     this.dispatchEvent(myEvent);
+  }
+
+  footerHandler(){
+    console.log('Bubble Event Called')
   }
 }
